dc600db34a7ae2cea6c05d1b5f9c45b3853b8566
Fernando Sena <fernandosenacruz@gmail.com>
2022-03-28 19:39:28 -0300

    1

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const constrollers = require('./controller');
 const middlewares = require('./middleware');
